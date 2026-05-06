f4a0054f51353d492cb88255284ba7d0d277b886
Bongkel66 <antonibudi066@gmail.com>
2026-05-06 18:34:46 +0700

    fix cors railway

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',     
-  database: 'managestore',
-  port: 3307        
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 })
 
 export default db
--- server.js
+++ server.js
@@ -4,12 +4,16 @@ import syncRoutes from './routes/sync.js'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}))
+
 app.use(express.json())
 
 app.use('/api/sync', syncRoutes)
 
-// 🔥 pakai port dari Railway
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, '0.0.0.0', () => {
